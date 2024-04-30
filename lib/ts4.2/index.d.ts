87e8f8d2412c58f171b1c21e1a0599f486a92a11
asllanmakaj7 <animakaj7@gmail.com>
2024-04-30 10:58:59 +0200

    chore: Removed extra files from commit

--- lib/ts4.2/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { MockGenerator, ShkoOnline } from "@shko.online/componentframework-mock";
-declare const mockLookupObjects: <TInputs extends ShkoOnline.PropertyTypes<TInputs>, TOutputs extends ShkoOnline.KnownTypes<TOutputs>>(mockGenerator: MockGenerator<TInputs, TOutputs>) => void;
-export default mockLookupObjects;
-//# sourceMappingURL=index.d.ts.map
